287b56afe0b7565a14ff10addcc214b6561666ac
dell <dell@DESKTOP-OB7FVAR>
2023-11-26 08:35:40 +0300

    Add project files.

---
+++ frmSevenSegment.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace SevenSegment
+{
+    partial class frmSevenSegment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSevSeg7 = new System.Windows.Forms.Button();
+            this.btnSevSeg1 = new System.Windows.Forms.Button();
+            this.btnSevSeg3 = new System.Windows.Forms.Button();
+            this.btnSevSeg2 = new System.Windows.Forms.Button();
+            this.btnSevSeg4 = new System.Windows.Forms.Button();
+            this.btnSevSeg6 = new System.Windows.Forms.Button();
+            this.btnSevSeg5 = new System.Windows.Forms.Button();
+            this.gbNumbers = new System.Windows.Forms.GroupBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.gbNumbers.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSevSeg7
+            // 
+            this.btnSevSeg7.Enabled = false;
+            this.btnSevSeg7.Location = new System.Drawing.Point(249, 40);
+            this.btnSevSeg7.Name = "btnSevSeg7";
+            this.btnSevSeg7.Size = new System.Drawing.Size(29, 147);
+            this.btnSevSeg7.TabIndex = 0;
+            this.btnSevSeg7.UseVisualStyleBackColor = true;
+            // 
+            // btnSevSeg1
+            // 
+            this.btnSevSeg1.Enabled = false;
+            this.btnSevSeg1.Location = new System.Drawing.Point(284, 22);
+            this.btnSevSeg1.Name = "btnSevSeg1";
+            this.btnSevSeg1.Size = new System.Drawing.Size(145, 26);
+            this.btnSevSeg1.TabIndex = 1;
+            this.btnSevSeg1.UseVisualStyleBackColor = true;
+            // 
+            // btnSevSeg3
+            // 
+            this.btnSevSeg3.Enabled = false;
+            this.btnSevSeg3.Location = new System.Drawing.Point(284, 183);
+            this.btnSevSeg3.Name = "btnSevSeg3";
+            this.btnSevSeg3.Size = new System.Drawing.Size(145, 26);
+            this.btnSevSeg3.TabIndex = 2;
+            this.btnSevSeg3.UseVisualStyleBackColor = true;
+            // 
+            // btnSevSeg2
+            // 
+            this.btnSevSeg2.Enabled = false;
+            this.btnSevSeg2.Location = new System.Drawing.Point(435, 40);
+            this.btnSevSeg2.Name = "btnSevSeg2";
+            this.btnSevSeg2.Size = new System.Drawing.Size(29, 147);
+            this.btnSevSeg2.TabIndex = 3;
+            this.btnSevSeg2.UseVisualStyleBackColor = true;
+            // 
+            // btnSevSeg4
+            // 
+            this.btnSevSeg4.Enabled = false;
+            this.btnSevSeg4.Location = new System.Drawing.Point(435, 206);
+            this.btnSevSeg4.Name = "btnSevSeg4";
+            this.btnSevSeg4.Size = new System.Drawing.Size(29, 147);
+            this.btnSevSeg4.TabIndex = 4;
+            this.btnSevSeg4.UseVisualStyleBackColor = true;
+            // 
+            // btnSevSeg6
+            // 
+            this.btnSevSeg6.Enabled = false;
+            this.btnSevSeg6.Location = new System.Drawing.Point(249, 206);
+            this.btnSevSeg6.Name = "btnSevSeg6";
+            this.btnSevSeg6.Size = new System.Drawing.Size(29, 147);
+            this.btnSevSeg6.TabIndex = 5;
+            this.btnSevSeg6.UseVisualStyleBackColor = true;
+            // 
+            // btnSevSeg5
+            // 
+            this.btnSevSeg5.Enabled = false;
+            this.btnSevSeg5.Location = new System.Drawing.Point(284, 347);
+            this.btnSevSeg5.Name = "btnSevSeg5";
+            this.btnSevSeg5.Size = new System.Drawing.Size(145, 26);
+            this.btnSevSeg5.TabIndex = 6;
+            this.btnSevSeg5.UseVisualStyleBackColor = true;
+            // 
+            // gbNumbers
+            // 
+            this.gbNumbers.Controls.Add(this.btn0);
+            this.gbNumbers.Controls.Add(this.btn9);
+            this.gbNumbers.Controls.Add(this.btn8);
+            this.gbNumbers.Controls.Add(this.btn7);
+            this.gbNumbers.Controls.Add(this.btn6);
+            this.gbNumbers.Controls.Add(this.btn5);
+            this.gbNumbers.Controls.Add(this.btn4);
+            this.gbNumbers.Controls.Add(this.btn);
+            this.gbNumbers.Controls.Add(this.btn2);
+            this.gbNumbers.Controls.Add(this.btn1);
+            this.gbNumbers.Font = new System.Drawing.Font("Lucida Sans Unicode", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbNumbers.ForeColor = System.Drawing.Color.Gray;
+            this.gbNumbers.Location = new System.Drawing.Point(163, 394);
+            this.gbNumbers.Name = "gbNumbers";
+            this.gbNumbers.Size = new System.Drawing.Size(398, 319);
+            this.gbNumbers.TabIndex = 7;
+            this.gbNumbers.TabStop = false;
+            this.gbNumbers.Text = "Numbers";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(87, 79);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(51, 46);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(172, 79);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(51, 46);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(257, 79);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(51, 46);
+            this.btn.TabIndex = 2;
+            this.btn.Text = "3";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(87, 137);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(51, 46);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(172, 135);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(51, 46);
+            this.btn5.TabIndex = 5;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(257, 137);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(51, 46);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(87, 195);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(51, 46);
+            this.btn7.TabIndex = 6;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(172, 191);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(51, 46);
+            this.btn8.TabIndex = 7;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(257, 195);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(51, 46);
+            this.btn9.TabIndex = 8;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(172, 247);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(51, 46);
+            this.btn0.TabIndex = 9;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // frmSevenSegment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 749);
+            this.Controls.Add(this.gbNumbers);
+            this.Controls.Add(this.btnSevSeg5);
+            this.Controls.Add(this.btnSevSeg6);
+            this.Controls.Add(this.btnSevSeg4);
+            this.Controls.Add(this.btnSevSeg2);
+            this.Controls.Add(this.btnSevSeg3);
+            this.Controls.Add(this.btnSevSeg1);
+            this.Controls.Add(this.btnSevSeg7);
+            this.Name = "frmSevenSegment";
+            this.Text = "Seven Segment";
+            this.gbNumbers.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSevSeg7;
+        private System.Windows.Forms.Button btnSevSeg1;
+        private System.Windows.Forms.Button btnSevSeg3;
+        private System.Windows.Forms.Button btnSevSeg2;
+        private System.Windows.Forms.Button btnSevSeg4;
+        private System.Windows.Forms.Button btnSevSeg6;
+        private System.Windows.Forms.Button btnSevSeg5;
+        private System.Windows.Forms.GroupBox gbNumbers;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Button btn2;
+    }
+}
+
---
+++ frmSevenSegment.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SevenSegment
+{
+    public partial class frmSevenSegment : Form
+    {
+        public frmSevenSegment()
+        {
+            InitializeComponent();
+        }
+
+        void ResetSevSegButtons()
+        {
+            btnSevSeg1.BackColor = Color.White;
+            btnSevSeg2.BackColor = Color.White;
+            btnSevSeg3.BackColor = Color.White;
+            btnSevSeg4.BackColor = Color.White;
+            btnSevSeg5.BackColor = Color.White;
+            btnSevSeg6.BackColor = Color.White;
+            btnSevSeg7.BackColor = Color.White;
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            ResetSevSegButtons();
+            btnSevSeg2.BackColor = Color.GreenYellow;
+            btnSevSeg4.BackColor = Color.GreenYellow;
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            ResetSevSegButtons();
+            btnSevSeg1.BackColor = Color.GreenYellow;
+            btnSevSeg2.BackColor = Color.GreenYellow;
+            btnSevSeg3.BackColor = Color.GreenYellow;
+            btnSevSeg5.BackColor = Color.GreenYellow;
+            btnSevSeg6.BackColor = Color.GreenYellow;
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            ResetSevSegButtons();
+            btnSevSeg1.BackColor = Color.GreenYellow;
+            btnSevSeg2.BackColor = Color.GreenYellow;
+            btnSevSeg3.BackColor = Color.GreenYellow;
+            btnSevSeg5.BackColor = Color.GreenYellow;
+            btnSevSeg4.BackColor = Color.GreenYellow;
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            ResetSevSegButtons();
+            btnSevSeg2.BackColor = Color.GreenYellow;
+            btnSevSeg3.BackColor = Color.GreenYellow;
+            btnSevSeg4.BackColor = Color.GreenYellow;
+            btnSevSeg7.BackColor = Color.GreenYellow;
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            ResetSevSegButtons();
+            btnSevSeg1.BackColor = Color.GreenYellow;
+            btnSevSeg7.BackColor = Color.GreenYellow;
+            btnSevSeg3.BackColor = Color.GreenYellow;
+            btnSevSeg4.BackColor = Color.GreenYellow;
+            btnSevSeg5.BackColor = Color.GreenYellow;
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            ResetSevSegButtons();
+            btnSevSeg1.BackColor = Color.GreenYellow;
+            btnSevSeg7.BackColor = Color.GreenYellow;
+            btnSevSeg3.BackColor = Color.GreenYellow;
+            btnSevSeg5.BackColor = Color.GreenYellow;
+            btnSevSeg4.BackColor = Color.GreenYellow;
+            btnSevSeg6.BackColor = Color.GreenYellow;
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            ResetSevSegButtons();
+            btnSevSeg1.BackColor = Color.GreenYellow;
+            btnSevSeg2.BackColor = Color.GreenYellow;
+            btnSevSeg4.BackColor = Color.GreenYellow;
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            ResetSevSegButtons();
+            btnSevSeg1.BackColor = Color.GreenYellow;
+            btnSevSeg2.BackColor = Color.GreenYellow;
+            btnSevSeg3.BackColor = Color.GreenYellow;
+            btnSevSeg4.BackColor = Color.GreenYellow;
+            btnSevSeg5.BackColor = Color.GreenYellow;
+            btnSevSeg6.BackColor = Color.GreenYellow;
+            btnSevSeg7.BackColor = Color.GreenYellow;
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            ResetSevSegButtons();
+            btnSevSeg1.BackColor = Color.GreenYellow;
+            btnSevSeg2.BackColor = Color.GreenYellow;
+            btnSevSeg3.BackColor = Color.GreenYellow;
+            btnSevSeg4.BackColor = Color.GreenYellow;
+            btnSevSeg5.BackColor = Color.GreenYellow;
+            btnSevSeg7.BackColor = Color.GreenYellow;
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            ResetSevSegButtons();
+            btnSevSeg1.BackColor = Color.GreenYellow;
+            btnSevSeg2.BackColor = Color.GreenYellow;
+            btnSevSeg4.BackColor = Color.GreenYellow;
+            btnSevSeg5.BackColor = Color.GreenYellow;
+            btnSevSeg6.BackColor = Color.GreenYellow;
+            btnSevSeg7.BackColor = Color.GreenYellow;
+        }
+    }
+}
